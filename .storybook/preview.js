e23eca36cb82a7e5f1b8f1193403b2e47de54e6c
Charles Howard <frodare@gmail.com>
2021-07-09 23:39:38 -0400

    updating to prerelease 7

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,5 @@
-const Provider = require('react-redux').Provider
 const store = require('../src/index').store
-const withRedux = require('addon-redux').withRedux
-
-const withReduxSettings = {
-  UserProvider: Provider,
-  store
-}
-
-const withReduxDecorator = withRedux(withReduxSettings)
 
 module.exports = {
-  decorators: [withReduxDecorator]
+  decorators: []
 };
